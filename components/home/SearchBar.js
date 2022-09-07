2bac82c0c93a146bab045f1fc97382dd3512e687
carlitoSwaaay <cacollier02@yahoo.com>
2022-09-07 15:32:49 -0700

    modified search.js

--- components/home/SearchBar.js
+++ components/home/SearchBar.js
@@ -8,7 +8,7 @@ export default function SearchBar({ cityHandler }) {
   return (
     <View style={{ marginTop: 15, flexDirection: "row" }}>
       <GooglePlacesAutocomplete
-        query={{ key: "" }}
+        query={{ key: "llll" }}
         onPress={(data, details = null) => {
           console.log(data.description);
           const city = data.description.split(",")[0];
